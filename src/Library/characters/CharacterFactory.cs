adb54f6dcc6e60b58d01b9c313f9bbeb4b672b62
ManuelBaldomir <manuel.baldomir@correo.ucu.edu.uy>
2022-10-31 20:23:58 -0300

    -

--- src/Library/characters/CharacterFactory.cs
+++ src/Library/characters/CharacterFactory.cs
@@ -6,7 +6,10 @@ namespace RoleplayGame.Characters
     public enum CharacterType
     {
         Elf = 1,
-        Wizard = 2
+        Wizard = 2,
+        Troll = 3,
+        Dwarf = 4,
+        Knight = 5,
     }
 
     /// <summary>
@@ -23,6 +26,9 @@ namespace RoleplayGame.Characters
             {
                 case CharacterType.Elf: return new Elf(name);
                 case CharacterType.Wizard: return new Wizard(name);
+
+                case CharacterType.Dwarf: return new Dwarf(name);
+                case CharacterType.Knight: return new Knight(name);
 
                 default: throw new System.Exception($"Invalid character {type}");
             }
